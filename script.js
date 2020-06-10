2782794e10b0d3bb8f3a90cebae30bb04118c127
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-10 10:01:56 -0700

    got timer working on start button click

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Variables associated with DOM elements
 const startButton = document.querySelector('#start');
 const welcomeElements = document.querySelectorAll('.welcome');
+const timer = document.querySelector('#timer');
 const quizElements = document.querySelectorAll('.quiz');
 const displayQuestion = document.querySelector('#questions')
 const choiceA = document.querySelector('#choiceA')
@@ -18,6 +19,21 @@ let questions = [
     {q: 'A very useful tool used during development and debugging for printing content to the debugger is:', o1: 'A) JavaScript', o2: 'B) terminal / bash', o3: 'C) for loops', o4: 'D) console.log', a: 'D) console.log'}
 ];
 
+const startTimer = function() {   
+    let secondsLeft = 60;
+    let secondsElapsed = 0;
+    timer.setAttribute('class', 'show timer');
+    let countdown = setInterval(function() {
+        secondsLeft--
+        let counter = timer.innerText = 'Seconds remaining: ' + secondsLeft;
+        console.log(secondsLeft);
+    }, 
+    1000)
+    if (secondsLeft === 55) {
+        countdown.clearInterval;
+    }
+};
+
 // Event listener that starts quiz by hiding all the .welcome class and showing the .quiz class; also starts the timer and the askQuestion() function
 startButton.addEventListener('click', function() {
     let i = 0;
@@ -31,34 +47,55 @@ startButton.addEventListener('click', function() {
         j++;
     };
     askQuestion();
-    // startTimer();
+    startTimer();
 });
 
 // Function that displays current question and multiple choice responses
 let currentQuestion = 0;
+console.log(questions[currentQuestion].a);
 const askQuestion = function() {
     displayQuestion.innerText = questions[currentQuestion].q;
     choiceA.textContent = questions[currentQuestion].o1;
     choiceB.textContent = questions[currentQuestion].o2;
-    choiceC.textContent = questions[currentQuestion].o3;
+    choiceC.innerHTML = questions[currentQuestion].o3;
     choiceD.textContent = questions[currentQuestion].o4;
 }
 
 /* const checkAnswer = function() {
     if ()
 } */
-
+console.log(choiceC.innerText);
 choiceA.addEventListener('click', function() {
-    console.log('hi');
+    if (questions[currentQuestion].o1 === questions[currentQuestion].a) {
+        console.log('correct')
+    }
+    else {
+        console.log('incorrect')
+    }
 });
 choiceB.addEventListener('click', function() {
-    console.log('hi');
+    if (choiceA.textContent === questions[currentQuestion].a.textContent) {
+        console.log('correct')
+    }
+    else {
+        console.log('incorrect')
+    }
 });
 choiceC.addEventListener('click', function() {
-    console.log('hi');
+    if (choiceA.textContent === questions[currentQuestion].a.textContent) {
+        console.log('correct')
+    }
+    else {
+        console.log('incorrect')
+    }
 });
 choiceD.addEventListener('click', function() {
-    console.log('hi');
+    if (choiceA.textContent === questions[currentQuestion].a.textContent) {
+        console.log('correct')
+    }
+    else {
+        console.log('incorrect')
+    }
 });
 
 
