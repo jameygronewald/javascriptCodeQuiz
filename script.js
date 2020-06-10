45121a6ad6f1ed8509f13eeebb6fd3a8335072ed
Jamey Gronewald <jrgronewald@gmail.com>
2020-06-09 20:51:09 -0700

    updated array of questions and answers to include real JS questions; added  click event to start button that hides welcome screen; separated HTML out into 3 main divs that will hold the different elements that will show or hide at different stages of app

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 // Variables associated with DOM elements
-const highScores = document.querySelector('#highScores');
-const timer = document.querySelector('#timer');
-const score = document.querySelector('#score');
-const displayQuestion = document.querySelector('#questions');
+const startButton = document.querySelector('#start');
+const welcomeElements = document.querySelectorAll('.welcome');
+
 
 // Array of quiz questions
 let questions = [
-    {q: 'What is your name?', o1: 'A) Larry', o2: 'B) Per', o3: 'C) Jamey', o4: 'D) Martin', a: 'C) Jamey'},
-    {q: 'What is your age?', o1: 'A) 34', o2: 'B) 28', o3: 'C) 29', o4: 'D) 58', a: 'B) 28'},
-    {q: 'Where do you live?', o1: 'A) Seattle', o2: 'B) Caracas', o3: 'C) Cape Town', o4: 'D) Minsk', a: 'A) Seattle'}
+    {q: 'Commonly used data types DO NOT include:', o1: 'A) strings', o2: 'B) booleans', o3: 'C) alerts', o4: 'D) numbers', a: 'C) alerts'},
+    {q: 'The condition in an if/else statement is enclosed within _____.', o1: 'A) quotes', o2: 'B) curly brackets', o3: 'C) parentheses', o4: 'D) square brackets', a: 'C) parentheses'},
+    {q: 'Arrays in JavaScript can be used to store _____.', o1: 'A) numbers and strings', o2: 'B) other arrays', o3: 'C) booleans', o4: 'D) all of the above', a: 'D) all of the above'},
+    {q: 'String values must be enclosed within ______ when being assigned to variables.', o1: 'A) commas', o2: 'B) curly brackets', o3: 'C) quotes', o4: 'D) parentheses', a: 'C) quotes'},
+    {q: 'A very useful tool used during development and debugging for printing content to the debugger is:', o1: 'A) JavaScript', o2: 'B) terminal / bash', o3: 'C) for loops', o4: 'D) console.log', a: 'D) console.log'}
 ];
 
 let score = 0;
 let currentQuestion = 0;
 
-
+// Event listener that starts quiz by hiding all the .welcome class and showing the .quiz class; also starts the timer and the questionAsk() function
+startButton.addEventListener('click', function(event) {
+    event.preventDefault();
+    let i = 0;
+    while (i < welcomeElements.length) {
+        welcomeElements[i].setAttribute('class', 'hide');
+        i++;
+    }
+    // startTimer();
+});
